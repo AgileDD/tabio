f7c4e323062c0a8b16dba8aa24ca56368365e262
Brian Hoffman <brian.hoffman@agiledd.com>
2020-01-20 22:37:06 -0500

    Add code to split a page mask into lines
    This will be used to build a line classifier with the
    split mask portion being used as the features.

--- mask.py
+++ mask.py
@@ -8,6 +8,10 @@
 import sys
 import csv_file
 from PIL import Image, ImageDraw
+import statistics
+from scipy.signal import decimate
+import numpy as np
+
 
 def create(lines):
     max_x=0
@@ -35,14 +39,46 @@ def create(lines):
 
     #resize the mask so each pixel represents about 1 character
     char_width, char_height = csv_file.mean_char_size(lines)
-    new_width = (int)(1.0 * mask.width / char_width)
-    new_height = (int)(1.0 * mask.height / char_height)
+    x_scale = 1.0 / char_width
+    y_scale = 1.0 / char_height
+    new_width = (int)(mask.width * x_scale)
+    new_height = (int)(mask.height * y_scale)
 
     mask = mask.resize((new_width, new_height), resample=Image.BICUBIC)
-    return mask
+    #mask = Image.fromarray(np.uint8(decimate(mask, int(char_width) * 255)))
+    return (mask, (x_scale, y_scale))
+
+
+# split the mask into a mask image per line
+# each line mask will include context above and below the line
+def split(mask, scale, lines):
+    mean_line_height = statistics.mean(
+        map(lambda line: line.bbox.bottom - line.bbox.top, lines))
+    width = mask.width
+
+    masks = []
+
+    for line in lines:
+        top = line.bbox.top - (mean_line_height * 5.0)
+        bottom = line.bbox.bottom + (mean_line_height * 5.0)
+        box = (
+            0,
+            max(0, int(top * scale[1])),
+            width,
+            min(mask.height, int(bottom * scale[1])))
+
+        masks.append(mask.crop(box))
+
+    return masks
+
 
 if __name__ == '__main__':
     fname = sys.argv[1]
     lines = csv_file.read_csv(fname)
-    mask = create(lines)
+    mask, scale = create(lines)
     mask.save('mask.png', 'PNG')
+
+    masks = split(mask, scale, lines)
+    for i, m in enumerate(masks):
+        #print(i)
+        m.save('mask-'+str(i)+'.png', 'PNG')
