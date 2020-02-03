996831f5b9d408cd8dcbbc12689715e22c6365b5
Brian Hoffman <brian.hoffman@agiledd.com>
2020-02-03 17:58:26 -0500

    Generate features from labeled data
    Given csv files and labels in xml files generate a character mask
    per page, then  split the mask up per line. The masks generated will
    be used for features for a line classifier. Double columns are also split
    based on the labels. The output features will not span 2 columns.
    
    Usage: python split_lines.py <path to input files> <output training dir> <output test dir> <path to file containing list of hashes for test>

--- split_lines.py
+++ split_lines.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import errno
+from glob import glob
+import os.path
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -11,24 +13,6 @@ import pascalvoc
 import mask
 import csv_file
 
-matplotlib.use('GTK3Cairo')
-
-#doc_hash = '99e91ba482e98894f5c8da17dde5b259'
-#doc_csv_hash = 'baad73b47f22e16520e04b02455c5817'
-
-doc_hash = sys.argv[1]
-page_number = int(sys.argv[2])
-base_fname = '../labeled-data/IFP Samples/'+doc_hash+'_'+str(page_number)+'_'+str(page_number)
-#base_csv_fname = '../labeled-data/Tarfaya/'+doc_csv_hash+'_'+str(page_number)+'_'+str(page_number)
-base_csv_fname = base_fname
-
-
-lines = csv_file.read_csv(base_csv_fname+'.csv')
-spacial_lines = csv_file.remove_margin(csv_file.group_lines_spacially(lines))
-labeled_boxes = pascalvoc.read(base_fname+'.xml')
-
-doc_mask = mask.create(spacial_lines)
-masks = mask.split(doc_mask, spacial_lines)
 
 def classify_line(line):
     for bbox in line.bboxes:
@@ -64,56 +48,6 @@ def split_line(line, width):
 #fig, ax = plt.subplots(1)
 #ax.imshow(im)
 
-(width, _) = csv_file.size(spacial_lines)
-
-# split up lines, labels and masks further by column
-# single column entries can be carried over as is, but
-# double column entries will be split in half and both halfs added
-# to the following lists
-split_lines = []
-split_labels = []
-split_masks = []
-
-for line, mask in zip(spacial_lines, masks):
-
-        line_category = classify_line(line)
-        if line_category is None:
-            continue
-
-        #print(line_category)
-
-        column_type, label = line_category.split('-')
-
-        if column_type == 'SingleColumn':
-            split_lines.append(line)
-            split_labels.append(label)
-            split_masks.append(mask)
-            print(label)
-        if column_type == 'DoubleColumn':
-            #split line and mask in half
-            l,r = split_line(line, width/2.0)
-
-            mask_cut_point = mask.width / 2
-            lmask = mask.crop((0, 0, mask_cut_point, mask.height))
-            rmask = mask.crop((mask_cut_point, 0, mask.width, mask.height))
-
-            if l is not None:
-                l_cat = classify_line(l).split('-')[1]
-                if l_cat is not None:
-                    split_lines.append(l)
-                    split_labels.append(l_cat)
-                    split_masks.append(lmask)
-                    print('Left -'+split_labels[-1])
-            if r is not None:
-                r_cat = classify_line(r).split('-')[1]
-                if r_cat is not None:
-                    split_lines.append(r)
-                    split_labels.append(r_cat)
-                    split_masks.append(rmask)
-                    print('Right-'+split_labels[-1])
-
-
-
 #csv_file.draw(split_lines, ax)
 
 #pascalvoc.draw(labeled_boxes, ax)
@@ -127,10 +61,111 @@ def mkdir(dir):
         if e.errno != errno.EEXIST:
             raise
 
-out_dir = 'sorted_masks'
-mkdir(out_dir)
 
-for i, (line, label, mask) in enumerate(zip(split_lines, split_labels, split_masks)):
-    mkdir(f"{out_dir}/{label}")
-    out_mask = mask.resize((100, 20), resample=Image.BICUBIC)
-    out_mask.save(f"{out_dir}/{label}/{doc_hash}_{page_number}_{i}.png", 'PNG')
+# creates line features for a given page in a document
+def create_features(lines, labeled_boxes):
+    doc_mask = mask.create(lines)
+    masks = mask.split(doc_mask, lines)
+
+    (width, _) = csv_file.size(lines)
+
+    split_lines = []
+    split_labels = []
+    split_masks = []
+
+    for line, whole_mask in zip(lines, masks):
+
+            line_category = classify_line(line)
+            if line_category is None:
+                continue
+
+            #print(line_category)
+
+            column_type, label = line_category.split('-')
+
+            if column_type == 'SingleColumn':
+                split_lines.append(line)
+                split_labels.append(label)
+                split_masks.append(whole_mask)
+                print(label)
+            if column_type == 'DoubleColumn':
+                #split line and mask in half
+                l,r = split_line(line, width/2.0)
+
+                mask_cut_point = whole_mask.width / 2
+                lmask = whole_mask.crop((0, 0, mask_cut_point, whole_mask.height))
+                rmask = whole_mask.crop((mask_cut_point, 0, whole_mask.width, whole_mask.height))
+
+                if l is not None:
+                    l_cat = classify_line(l)
+                    if l_cat is not None:
+                        l_cat = l_cat.split('-')[1]
+                        split_lines.append(l)
+                        split_labels.append(l_cat)
+                        split_masks.append(lmask)
+                        print('Left -'+split_labels[-1])
+                if r is not None:
+                    r_cat = classify_line(r)
+                    if r_cat is not None:
+                        r_cat = r_cat.split('-')[1]
+                        split_lines.append(r)
+                        split_labels.append(r_cat)
+                        split_masks.append(rmask)
+                        print('Right-'+split_labels[-1])
+
+    features = map(lambda m: m.resize((100, 20), resample=Image.BICUBIC), split_masks)
+
+    return (split_lines, features, split_labels)
+
+
+def find_matching_csv(label_fname):
+    simple_csv_fname = os.path.splitext(label_fname)[0]+'.csv'
+    if os.path.exists(simple_csv_fname):
+        return simple_csv_fname
+
+    # part of the page deosn't match. Label files have two pages numbers but only 1 matters
+    dir_name = os.path.dirname(label_fname)
+    (doc_hash, _, page_number) = os.path.splitext(os.path.basename(label_fname))[0].split('_')
+
+    single_page_fname = os.path.join(dir_name, doc_hash+'_'+str(page_number)+'.csv')
+    if os.path.exists(single_page_fname):
+        return single_page_fname
+
+    double_page_fname = os.path.join(dir_name, doc_hash+'_'+str(page_number)+'_'+str(page_number)+'.csv')
+    if os.path.exists(double_page_fname):
+        return double_page_fname
+
+    return None
+
+
+
+if __name__ == '__main__':
+    in_dir = sys.argv[1]
+    train_dir = sys.argv[2]
+    test_dir = sys.argv[3]
+    test_hashes = set([line.strip() for line in open(sys.argv[4])])
+
+    label_files = glob(f'{in_dir}/*.xml')
+    csv_files = map(find_matching_csv, label_files)
+
+    mkdir(test_dir)
+    mkdir(train_dir)
+
+    for (label_fname, csv_fname) in zip(label_files, csv_files):
+        if csv_fname is None:
+            continue
+
+        (doc_hash, _, page_number) = os.path.splitext(os.path.basename(csv_fname))[0].split('_')
+        print((doc_hash, page_number, label_fname, csv_fname))
+
+        lines = csv_file.read_csv(csv_fname)
+        lines = csv_file.remove_margin(csv_file.group_lines_spacially(lines))
+        labeled_boxes = pascalvoc.read(label_fname)
+
+        (_, features, labels) = create_features(lines, labeled_boxes)
+
+        out_dir = test_dir if doc_hash in test_hashes else train_dir
+
+        for i, (feature, label) in enumerate(zip(features, labels)):
+            mkdir(f"{out_dir}/{label}")
+            feature.save(f"{out_dir}/{label}/{doc_hash}_{page_number}_{i}.png", 'PNG')
