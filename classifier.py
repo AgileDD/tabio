87f18c41bece88d52844aaea517b83759c73c0e2
Brian Hoffman <brian.hoffman@agiledd.com>
2020-02-03 20:44:17 -0500

    Read generated features during classification
    Use features generated from split_lines.py during classification. The containing
    folder of each feature image defines the ground truth.

--- classifier.py
+++ classifier.py
@@ -3,14 +3,25 @@ import numpy as np
 from torch.utils import data
 from torch import nn
 import torch.nn.functional as F
+from PIL import Image
+from glob import glob
 
-# generate 1000 samples of size 10X100
-Samples = 1000
-M = 10
+M = 20
 N = 100
 
-train_features = np.random.rand(Samples,M,N)
-train_targets = np.random.randint(0,1,Samples)
+def read_feature(fname):
+  return np.array(Image.open(fname), dtype=np.uint8)
+
+paragraph_features = glob('train/Paragraph/*.png')
+sparce_features = glob('train/Sparse/*.png')
+
+train_features = np.array(list(map(read_feature, paragraph_features))+list(map(read_feature, sparce_features)))/128.0
+train_targets = np.array([1]*len(paragraph_features) + [0]*len(sparce_features))
+
+print(train_targets.shape)
+print(train_features.shape)
+
+Samples = len(train_targets)
 
 torch_train_X = torch.from_numpy(train_features)
 torch_train_Y = torch.from_numpy(train_targets)
@@ -18,7 +29,7 @@ torch_train_Y = torch.from_numpy(train_targets)
 train_dataset = data.TensorDataset(torch_train_X,torch_train_Y)
 train_dataloader = data.DataLoader(train_dataset)
 
-model = nn.Sequential(nn.Linear(1000, 128),
+model = nn.Sequential(nn.Linear(M*N, 128),
                       nn.ReLU(),
                       nn.Linear(128, 64),
                       nn.ReLU(),
