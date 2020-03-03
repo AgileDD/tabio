ca9a8b1da5676ef6df16b05c2fab3e0f5924a92c
Amit Juneja <amit.juneja@agiledd.com>
2020-03-03 11:08:19 -0600

    Multi-class and some fixes to make it converge.

--- classifier.py
+++ classifier.py
@@ -5,6 +5,7 @@ from torch import nn
 import torch.nn.functional as F
 from PIL import Image
 from glob import glob
+import sys
 
 M = 20
 N = 100
@@ -12,14 +13,23 @@ N = 100
 def read_feature(fname):
   return np.array(Image.open(fname), dtype=np.uint8)
 
-paragraph_features = glob('train/Paragraph/*.png')
-sparce_features = glob('train/TableSparseMulticolumn/*.png')
 
-train_features = np.array(list(map(read_feature, paragraph_features))+list(map(read_feature, sparce_features)))/128.0
-train_targets = np.array([1]*len(paragraph_features) + [0]*len(sparce_features))
+classes = glob('/home/brian/line_data/train/*')
+classes = [c.split("/")[-1] for c in classes]
+print(classes)
+train_features = []
+train_targets = []
+for label,cl in enumerate(classes):
+    features = glob("/home/brian/line_data/train/"+cl+"/*.png")
+    targets = [label]*len(features)
+    train_features = train_features + list(map(read_feature, features))
+    train_targets = train_targets + targets
+train_features = np.array(train_features)/128.0
+train_targets = np.array(train_targets)
 
 print(train_targets.shape)
 print(train_features.shape)
+
 
 Samples = len(train_targets)
 
@@ -27,20 +37,44 @@ torch_train_X = torch.from_numpy(train_features)
 torch_train_Y = torch.from_numpy(train_targets)
 
 train_dataset = data.TensorDataset(torch_train_X,torch_train_Y)
-train_dataloader = data.DataLoader(train_dataset)
+train_dataloader = data.DataLoader(train_dataset,batch_size=200,shuffle=True)
+
+def weights_init_normal(m):
+        '''Takes in a module and initializes all linear layers with weight
+           values taken from a normal distribution.'''
+
+        classname = m.__class__.__name__
+        # for every Linear layer in a model
+        if classname.find('Linear') != -1:
+            y = m.in_features
+        # m.weight.data shoud be taken from a normal distribution
+            m.weight.data.normal_(0.0,1/np.sqrt(y))
+        # m.bias.data should be 0
+            m.bias.data.fill_(0)
+
+def init_weights(m):
+        if type(m) == nn.Linear:
+                    torch.nn.init.xavier_uniform(m.weight)
+                    m.bias.data.fill_(0.01)
 
 model = nn.Sequential(nn.Linear(M*N, 128),
+                      nn.BatchNorm1d(128),
+                      nn.Dropout(0.2),
                       nn.ReLU(),
                       nn.Linear(128, 64),
+                      nn.BatchNorm1d(64),
+                      nn.Dropout(0.2),
                       nn.ReLU(),
-                      nn.Linear(64, 10),
+                      nn.Linear(64, 17),
                       nn.LogSoftmax(dim=1))# Define the loss
 model = model.double()
+model.apply(weights_init_normal)
 criterion = nn.NLLLoss()# Optimizers require the parameters to optimize and a learning rate
-optimizer = torch.optim.SGD(model.parameters(), lr=0.003)
-epochs = 10
+optimizer = torch.optim.SGD(model.parameters(), lr=0.0003)
+epochs = 200
 for e in range(epochs):
     running_loss = 0
+    loss_len = 0
     for images, labels in train_dataloader:
         # Flatten MNIST images into a 784 long vector
         images = images.view(images.shape[0], -1)
@@ -49,11 +83,15 @@ for e in range(epochs):
         optimizer.zero_grad()
         
         output = model(images)
+        ### print output
+        ### print labels
         loss = criterion(output, labels)
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
-        print("Training loss: "+str(running_loss/len(train_dataloader)))
+        loss_len += 1
+    print("Training loss: "+str(running_loss/loss_len))
+    # print("Training loss: "+str(loss.item()))
 
 torch.save(model, './trained_net.pth')
 
--- test.py
+++ test.py
@@ -9,20 +9,36 @@ from glob import glob
 def read_feature(fname):
   return np.array(Image.open(fname), dtype=np.uint8)
 
-test_paragraph_features = glob('test/Paragraph/*.png')
-test_sparce_features = []#glob('test/TableSparseMulticolumn/*.png')
+classes = glob('/home/brian/line_data/test/*')
+classes = [c.split("/")[-1] for c in classes]
+print(classes)
+test_features = []
+test_targets = []
+for label,cl in enumerate(classes):
+    features = glob("/home/brian/line_data/test/"+cl+"/*.png")
+    targets = [label]*len(features)
+    test_features = test_features + list(map(read_feature, features))
+    test_targets = test_targets + targets
+test_features = np.array(test_features)/128.0
+test_targets = np.array(test_targets)
 
-test_features = np.array(list(map(read_feature, test_paragraph_features))+list(map(read_feature, test_sparce_features)))/128.0
-test_targets = np.array([1]*len(test_paragraph_features) + [0]*len(test_sparce_features))
+
+### test_paragraph_features = []#glob('/home/brian/line_data/test/Paragraph/*.png')
+### test_sparce_features =  glob('/home/brian/line_data/test/TableSparseMulticolumn/*.png')
+### 
+### test_features = np.array(list(map(read_feature, test_paragraph_features))+list(map(read_feature, test_sparce_features)))/128.0
+### test_targets = np.array([1]*len(test_paragraph_features) + [0]*len(test_sparce_features))
 
 torch_test_X = torch.from_numpy(test_features)
 torch_test_Y = torch.from_numpy(test_targets)
 
 test_dataset = data.TensorDataset(torch_test_X,torch_test_Y)
-test_dataloader = data.DataLoader(test_dataset)
+test_dataloader = data.DataLoader(test_dataset,batch_size=200)
 
 
 model = torch.load('./trained_net.pth')
+
+model.eval()
 
 correct = 0
 total = 0
@@ -36,7 +52,9 @@ for images, labels in test_dataloader:
     print('')
     total += labels.size(0)
     correct += (predicted == labels).sum().item()
+    print(labels)
+    print(predicted)
 
 print(correct)
 print(total)
-print(f'Accuracy: {100.0 * correct / total}%')
+# print('Accuracy: {100.0 * correct / total}%')
