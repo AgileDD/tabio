6af6f0e97de1b0b67ad89c26a30d48a4dca55cfb
Brian Hoffman <brian.hoffman@agiledd.com>
2020-05-14 18:00:51 -0400

    Add csv download script

---
+++ get_csv_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# download corresponding csv files to match hand labels in xml files
+# assumes csv and xml files are named like '<hash>_<page>_<page>.[csv|xml]'
+set -e
+
+target_dir=$1
+pushd "$target_dir"
+hashes=$(find -name '*.xml'|xargs -n1 basename|cut -d '_' -f 1|sort|uniq)
+popd
+
+for hash in $hashes
+do
+    python -c "import csv_file; csv_file.download('$hash', '$target_dir')"
+done
