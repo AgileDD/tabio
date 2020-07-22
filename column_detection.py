99001f73afe745bc87b2e529c3cc8e3ee1196ca0
Brian Hoffman <brian.hoffman@agiledd.com>
2020-07-22 13:20:18 -0400

    Move column detection eval code to column_detection.py
    This is part of the reorganization to consolidate column detection
    code into a single file.

--- column_detection.py
+++ column_detection.py
@@ -5,6 +5,7 @@ import numpy as np
 from torch.utils import data
 from torch import nn
 import torch.nn.functional as F
+from PIL import Image
 
 
 def read_line_classification(line, labeled_boxes):
@@ -53,3 +54,44 @@ class ColumnModel(nn.Module):
         for s in size:
             num_features *= s
         return num_features
+
+
+def detect_columns(masks):
+    ms = map(lambda m: m.resize((200, 20), resample=Image.BICUBIC), masks)
+    ms = list(map(np.array, ms))
+    labels = [0]*len(ms)
+    test_features = np.array(ms)/128.0
+    test_targets = np.array(labels)
+
+    print(test_targets.shape)
+    print(test_features.shape)
+
+
+    Samples = len(test_targets)
+
+    torch_test_X = torch.from_numpy(test_features)
+    torch_test_Y = torch.from_numpy(test_targets)
+
+    test_dataset = data.TensorDataset(torch_test_X,torch_test_Y)
+    test_dataloader = data.DataLoader(test_dataset,batch_size=10,shuffle=False)
+
+
+    model = torch.load('./col_trained_net.pth')
+
+    model.eval()
+
+    allhyp = []
+
+    for images, labels in test_dataloader:
+        images = images[:,None,:,:]
+        outputs = model(images)
+        print(outputs)
+
+        _, predicted = torch.max(outputs, 1)
+        for i in predicted:
+            print(i)
+        print('')
+        allhyp.extend(list(predicted))
+        print(predicted)
+
+    return allhyp
--- test_col_detector.py
+++ test_col_detector.py
@@ -4,13 +4,11 @@ from torch.utils import data
 from torch import nn
 from PIL import Image
 from glob import glob
-import model as modl
 import pickle
 from sklearn.metrics import confusion_matrix
 import data_loader
 import frontend
 import column_detection as cd
-import detect_columns
 import config
 
 all_col_labels = []
@@ -21,7 +19,7 @@ for page in list(data_loader.test_pages())[:100]:
 	lines = frontend.read_lines(page)
 	# def npmap(x): np.array(x, dtype=np.uint8)
 	masks = frontend.stage1(lines)
-	col_preds = detect_columns.detect_columns(masks)
+	col_preds = cd.detect_columns(masks)
 	
 	col_labs = [cd.fake_column_detection(l, labeled_boxes) for l in lines]
 	all_col_labels.extend(col_labs)
