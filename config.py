417a5146b6432921d8c0c8a3ab74a0e5aa8459c7
Brian Hoffman <brian.hoffman@agiledd.com>
2020-09-02 13:07:45 -0500

    Update test hash list
    Now that we have more data, add to the test set. Jose had used a bigger test set
    but most of that set was missing labels. This one has labels and is distributed evenly
    between the datasets.

--- config.py
+++ config.py
@@ -1,13 +1,33 @@
-in_dir = '/home/amit/experiments/tabio/SortedIFP/SortedIFP'
+in_dir = '/home/amit/experiments/tabio/all_links'
+
+
 test_hashes = [
-'8b19c56c096dbb5453e8993329914442',
+#Total:
+'744df57024cad2d7d5ce08b7bb44322e',
+'5684a5b404ca5e91dfecb791e8a901a3',
+
+#Technip:
+'623b8e806a4b324c0ed65ea2812a70fa',
+'4bfdba590339392cd2504d8bf5b3ad93',
+
+#Schlumberger:
+'6cc024556cf81d685fd045684a951210',
+
+#Sapiem:
+'12682288207b119847c5d550bf92df37',
+'d11fc8e706d28485e0045822c3601afb',
+
+#IFP:
 '30039718573b20e0bbc5f87121890534',
 '37ebadf81ad8bc694c8a3988582b8d2d',
 '2ec6960ba691c6dc7880cabeafce2129',
-# '122ef78a06236d02c1b6529245875a69',
 '32c1eb91db6a924e1a30a7c78bdd4ab0',
 '128a980c236d85c867f81557e34cb7f3',
-'20bceabf938737361888dc718db02c8c'
+'20bceabf938737361888dc718db02c8c',
+'42c8f8a90bc9a6e37f9de4b055421ba8',
+'1d3a3d5e43d75aea35aafcaf09687480',
+'072e99c74d60aa6c9fb3fd5992b8c239',
+'8b19c56c096dbb5453e8993329914442',
 ]
 classes = ['Author', 'Equation', 'FigureCaption', 'FigureText', 'Heading', 'PageFooter', 'PageHeader', 'PageNumber', 'Paragraph', 'References', 'Sparse', 'Subtitle', 'TableCaption', 'TableFooter', 'TableSparseColumnHeader', 'TableSparseMulticolumn', 'Title']
 col_classes = {"SingleColumn":0,"DoubleColumn":1,"None":2,"DoublColumn":1,None:2}
